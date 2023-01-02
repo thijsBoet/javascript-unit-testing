03e0e77125292fe9e92907aef7a58e04ee6ba288
thijsBoet <m.boet@chello.nl>
2023-01-02 05:11:29 +0100

    put same test files in backend node/esmodules projects

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,37 +1,47 @@
 import { validateStringNotEmpty, validateNumber } from './validation';
 
-it('should trow an error, if an empty string is provided', () => {
-    const input = '';
-    const result = () => validateStringNotEmpty(input);
-    expect(result).toThrow('Invalid input - must not be empty.');
+describe('validateStringNotEmpty()', () => {
+	it('should trow an error, if an empty string is provided', () => {
+		const input = '';
+		const validationFn = () => validateStringNotEmpty(input);
+		expect(validationFn).toThrow('Invalid input - must not be empty.');
+	});
+
+	it('should throw an error if a string is empty', () => {
+		const input = '';
+		const validationFn = () => validateStringNotEmpty(input);
+		expect(validationFn).toThrow('Invalid input - must not be empty.');
+	});
+
+	it('should trim spaces from input', () => {
+		const input = '  string  ';
+		const validationFn = () => validateStringNotEmpty(input);
+		expect(validationFn).not.toThrow();
+	});
 });
 
-it('should throw an error if a string is empty', () => {
-    const input = '';
-    const result = () => validateStringNotEmpty(input);
-    expect(result).toThrow('Invalid input - must not be empty.');
-});
+describe('validateNumber()', () => {
+	it('should throw an error if a number is NaN', () => {
+		const input = NaN;
+		const validationFn = () => validateNumber(input);
+		expect(validationFn).toThrow('Invalid number input.');
+    });
 
-it('should trim spaces from input', () => {
-    const input = '  string  ';
-    const result = () => validateStringNotEmpty(input);
-    expect(result).not.toThrow();
-});
+    it('should throw an error with a message that contains a reason (invalid number)', () => {
+        const input = 'a';
+        const validationFn = () => validateNumber(input);
+        expect(validationFn).toThrow('Invalid number input.');
+    });
 
-it('should throw an error if a number is NaN', () => {
-    const input = NaN;
-    const result = () => validateNumber(input);
-    expect(result).toThrow('Invalid number input.');
-});
+	it('should throw an error if a number is not a number', () => {
+		const input = 'string';
+		const validationFn = () => validateNumber(input);
+		expect(validationFn).toThrow('Invalid number input.');
+	});
 
-it('should throw an error if a number is not a number', () => {
-    const input = 'string';
-    const result = () => validateNumber(input);
-    expect(result).toThrow('Invalid number input.');
-});
-
-it('should not throw an error if a number is valid', () => {
-    const input = 1;
-    const result = () => validateNumber(input);
-    expect(result).not.toThrow();
+	it('should not throw an error if a number is valid', () => {
+		const input = 1;
+		const validationFn = () => validateNumber(input);
+		expect(validationFn).not.toThrow();
+	});
 });
